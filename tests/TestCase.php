1569c03c0022131a5dfde01704bcc469d8160f95
Klemen Tušar <techouse@gmail.com>
2019-02-05 22:39:14 +0100

    Removed obsolete dependencies

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Tests;
 
-use Faker\Factory;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 /**
@@ -20,24 +19,6 @@ abstract class TestCase extends BaseTestCase
      * @var \Faker\Generator
      */
     protected $faker;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->faker = $this->makeFaker();
-    }
-
-    /**
-     * Create a Faker instance for the given locale.
-     *
-     * @param  string $locale
-     * @return \Faker\Generator
-     */
-    protected function makeFaker($locale = null): \Faker\Generator
-    {
-        return Factory::create($locale ?? Factory::DEFAULT_LOCALE);
-    }
 
     /**
      * @param string $timezone
